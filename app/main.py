cc05ac0b745eb47a08bcf06a370c65cd6ebe83ff
Adi Jain <jainadi313@gmail.com>
2025-05-26 11:33:26 +0530

    pysqlite3 added

--- app/main.py
+++ app/main.py
@@ -8,6 +8,11 @@ import pandas as pd
 import chromadb
 import uuid
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 class Portfolio:
     def __init__(self, file_path="app/resource/my_portfolio.csv"):
         self.file_path = file_path
